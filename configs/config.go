819f30f899f9474024c761d55b0ef5ebb22e3e75
bom_d_van <bom.d.van@gmail.com>
2014-01-04 19:37:26 +0800

    using std log is already good enough

--- configs/config.go
+++ configs/config.go
@@ -1,12 +1,12 @@
 package configs
 
 import (
-	"github.com/bom-d-van/me/log"
+	"log"
 	"os"
 )
 
 var (
 	Port           = ":80"
-	Log            = log.NewLogger(os.Stdout, "", 0)
+	Log            = log.New(os.Stdout, "", log.LstdFlags|log.Lshortfile)
 	ReLoadTemplate = false
 )
